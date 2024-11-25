bff287ea72abb0017a506b97c6ccf032eb74a19a
Frank LSY <429598469@qq.com>
2024-11-25 11:48:37 -0600

    resident,process,test

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -1,4 +1,4 @@
-export OPENAI_API_KEY="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"
+export OPENAI_API_KEY=""
 export OPENAI_API_BASE="https://api.openai.com/v1/"
 python ../test.py --resident Agent.Resident.GPT --resident_openai_model_name gpt-3.5-turbo \
     --evaluator Agent.Evaluator.GPT --evaluator_openai_model_name gpt-3.5-turbo \
