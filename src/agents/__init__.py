8c6709ff1246759ca47ce9ba7d1e8018d85c76aa
Frank LSY <429598469@qq.com>
2024-12-02 15:45:03 -0600

    delete unneccessary

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,28 +1,12 @@
 from .base_agent import Agent
-# from .doctor import (
-#     Doctor, 
-#     GPTDoctor, 
-#     ChatGLMDoctor, 
-#     MinimaxDoctor, 
-#     WenXinDoctor, 
-#     QwenDoctor, 
-#     HuatuoGPTDoctor,
-#     HFDoctor
-# )
 from .doctor_gary import Doctor
 from .resident import Resident
-from .patient import Patient
 from .evaluator import Evaluator
-from .reporter import Reporter, ReporterV2
-from .host import Host
 
 
 __all__ = [
     "Agent",
     "Doctor",
-    # "GPTDoctor",
-    # "HFDoctor",
     "Resident",
     "Evaluator",
-    "Patient",
 ]   
